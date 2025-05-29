93f91e7a1ae762b54e4f09c6b9cd48536dd0a099
devHksingh <hksingh.dev@gmail.com>
2025-05-29 15:17:28 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,32 @@
-import express, { Request, Response } from "express";
+import express, {
+    NextFunction,
+    Request,
+    Response,
+} from "express";
+import logger from "./config/logger";
+import { HttpError } from "http-errors";
 
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Welcome to sever.");
 });
+
+// global error handler
+app.use(
+    (
+        err: HttpError,
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ) => {
+        logger.error(err.message);
+        const statusCode = err.statusCode || 500;
+        res.status(statusCode).json({
+            success: false,
+            msg: err.message,
+        });
+    },
+);
 
 export default app;
